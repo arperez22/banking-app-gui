4568ef2dd978aa2c3aecda21105dde7269934024
arperez22 <aperez21@huskers.unl.edu>
2024-12-12 09:17:58 -0600

    Fixed window size

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from gui import *
 
 def main():
     window = Tk()
-    window.title('Project 1')
+    window.title('Bank')
     window.geometry('400x400')
+    window.resizable(False, False)
 
     Gui(window)
 
